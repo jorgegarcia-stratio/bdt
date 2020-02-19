757543bdda9dfdd5347525c60a3c428442053db1
Antonio Lopez Navas <alopez@stratio.com>
2020-02-19 10:52:53 +0100

    [QATM-462] Remove import not necessary

--- src/main/java/com/stratio/qa/specs/CommandExecutionSpec.java
+++ src/main/java/com/stratio/qa/specs/CommandExecutionSpec.java
@@ -26,7 +26,6 @@ import org.assertj.core.api.Assertions;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Date;
 
